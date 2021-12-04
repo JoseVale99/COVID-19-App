ebc54ce364deaa14ab385ceff127892c4fc4cda4
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-12-04 04:32:42 -0600

    💥 NEW: footer for app

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; Covid Oaxaca 2021</div>
+            {{-- <div>
+                <a href="#">Datos de covid 2021</a>
+                &middot;
+                <a href="#">Terms &amp; Conditions</a>
+            </div> --}}
+        </div>
+    </div>
+</footer>
