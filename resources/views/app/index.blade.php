30d648b7c7111309beaeb35b4662ed820b6c5dd8
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-12-04 21:27:56 -0600

    ✅ Adding: desing app covid

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -28,7 +28,7 @@
                     <div class="row">
                         <div class="col-xl-3 col-md-6">
                             <div class="card bg-danger text-white mb-4">
-                                <div class="card-body text-center">Casos positivos a nivel nacional</div>
+                                <div class="card-body text-center">Total de casos positivos (México)</div>
                                 <div class="card-footer d-flex align-items-center justify-content-between">
                                     
                                      </div>
@@ -38,8 +38,8 @@
                             </div>
                         </div>
                         <div class="col-xl-3 col-md-6">
-                            <div class="card bg-danger text-white mb-4">
-                                <div class="card-body text-center">Casos positivos en el estado de Oaxaca</div>
+                            <div class="card bg-success text-white mb-4">
+                                <div class="card-body text-center">Total de casos positivos (Oaxaca)</div> 
                                 <div class="card-footer d-flex align-items-center justify-content-between">
                                     
                                      </div>
@@ -49,21 +49,18 @@
                             </div>
                         </div>
                         <div class="col-xl-3 col-md-6">
-                            <div class="card bg-success text-white mb-4">
-                                <div class="card-body">Success Card</div>
-                                <div class="card-footer d-flex align-items-center justify-content-between">
-                                    <a class="small text-white stretched-link" href="#">View Details</a>
-                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                </div>
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-body text-center">Total de casos positivos (CDMX)</div>
+                                <div class="card-footer d-flex align-items-center justify-content-between"></div>
+                                <h5 class="text-center"> {{$covid_mx_cases["Cases"]}}</h5>
                             </div>
                         </div>
                         <div class="col-xl-3 col-md-6">
-                            <div class="card bg-danger text-white mb-4">
-                                <div class="card-body">Danger Card</div>
-                                <div class="card-footer d-flex align-items-center justify-content-between">
-                                    <a class="small text-white stretched-link" href="#">View Details</a>
-                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                </div>
+                            <div class="card bg-dark text-white mb-4">
+                                <div class="card-body text-center">Total de casos positivos (Sta. Cat. Juquila)</div>
+                                <div class="card-footer d-flex align-items-center justify-content-between"></div>
+                                <h5 class="text-center"> {{$cases_data_juq["Accum Cases"]}}</h5>
+                                
                             </div>
                         </div>
                     </div>
